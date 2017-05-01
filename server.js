6e84a986bd8a119c18aab9b50d97daad7a1a97e3
dulajsan <dulaj.san@gmail.com>
2017-05-01 08:34:59 +0530

    temp

--- server.js
+++ server.js
@@ -17,5 +17,6 @@ app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
 app.use(logger('dev')); // Log requests to API using morgan
 app.use(cors());
+app.options('*', cors());
 
 router(app);
